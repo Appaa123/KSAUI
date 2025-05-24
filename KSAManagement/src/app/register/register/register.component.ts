1a50dcba13164ff15e04cd3a2a8a98b5c8aca7b9
appaarao22@gmail.com <appaarao22@gmail.com>
2025-05-25 01:59:15 +0530

    Added User registration code changes

--- KSAManagement/src/app/register/register/register.component.ts
+++ KSAManagement/src/app/register/register/register.component.ts
@@ -1,24 +1,34 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { SpinnerService } from '../../services/auth/spinner.service';
 import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 import { Router, RouterModule } from '@angular/router';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { Observable } from 'rxjs';
+import { catchError, Observable, tap } from 'rxjs';
+import { User } from '../../Models/User';
+import { error } from 'console';
+import { AuthComponent } from '../../auth/auth/auth.component';
 
 @Component({
   selector: 'app-register',
-  imports: [    CommonModule,
+  imports: [   
+     CommonModule,
     RouterModule,
     CommonModule,
     FormsModule,
-    NgxSpinnerModule],
+    NgxSpinnerModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  private token:any;
+  private tokenData:any;
+  private data:any;
+  noMatch:boolean = false;
+  private user:User = new User();
   formData = {
     Username:'',
     Password:'',
@@ -29,30 +39,101 @@ export class RegisterComponent {
     private http:HttpClient,
     private spinnerService: SpinnerService,
     private spinner: NgxSpinnerService, 
-    private router:Router, 
+    private router:Router,
     @Inject(PLATFORM_ID) private platformId: Object
   )
   {
   }
 
-  submitData() : Observable<any> {
-    return new Observable<any>();
+  getTokenData() : Observable<any> {
+
+    this.spinner.show();
+    this.tokenData = this.http.post<{token: string}>("https://ksaapi.onrender.com/api/Auth/login",
+      this.formData,
+      {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+      }
+    );
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+    }, 2000);
+    return this.tokenData;
   }
 
+
+  submitData() : Observable<any> {
+
+    const apiURL = "https://ksaapi.onrender.com/api/User/add-user";
+
+    if(this.formData.Password == this.formData.Re_enter_password){
+      this.user.Username = this.formData.Username;
+      this.user.Password = this.formData.Password; 
+    }
+    else{
+      this.noMatch = true;
+    }
+
+    // this.getTokenData()
+    //       .subscribe({
+    //         next: (response) => {
+    //           this.token = response.token; // ✅ Assign token here
+    //           console.log("Token received:", this.token);
+    //           alert("Form Submitted successfully");          
+    //           if (isPlatformBrowser(this.platformId)) {
+    //           // Optional: store in sessionStorage for future authenticated requests
+    //           sessionStorage.setItem('jwt', this.token);
+    //           sessionStorage.setItem('user', this.formData.Username);
+    //           }
+    //        this.spinner.show();
+    // //calling API to register user
+    //       this.data = this.http.post(apiURL, this.user, 
+    //           {
+    //             headers: { 
+    //               'Authorization': `Bearer ${this.token}`,
+    //               'Content-Type': 'application/json' 
+    //             }}).pipe(
+    //           tap(response => console.log("Success!", response)),
+    //           catchError(error => {
+    //             console.error('Error:', error);
+    //             throw error;
+    //           })
+    //         );
+    //       },
+    //   error: (error) => {
+    //     console.error('Login error:', error);
+    //   }
+    //   });
+
+    this.data = this.http.post(apiURL, this.user, 
+      {
+        headers: { 
+          'Content-Type': 'application/json' 
+        }}).pipe(
+      tap(response => console.log("Success!", response)),
+      catchError(error => {
+        console.error('Error:', error);
+        throw error;
+      })
+    );
+      return this.data;
+  } 
+
   onSubmit(form: NgForm) {
-    if(form.valid){
-    this.submitData()
-    .subscribe({
-      next: (response) => {
-        if (isPlatformBrowser(this.platformId)) {
-        // Optional: store in sessionStorage for future authenticated requests
-        sessionStorage.setItem('user', this.formData.Username);
+    if(form.valid && !this.noMatch){
+      this.submitData()
+      .subscribe({
+        next: (response) => {
+          if (isPlatformBrowser(this.platformId)) {
+          // Optional: store in sessionStorage for future authenticated requests
+          sessionStorage.clear();
+          this.router.navigate(['/auth']);
+          }
+        },
+        error: (error) => {
+          console.error('Login error:', error);
         }
-      },
-      error: (error) => {
-        console.error('Login error:', error);
-      }
-    });
+      });
   }
 }
 
